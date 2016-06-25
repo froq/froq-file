8ec54e04696bbff43f973a4aa1790b39b8a6a65e
Kerem Güneş <k-gun@mail.com>
2016-06-26 00:44:28 +0300

    Add tpl file.

--- src/Upload/File.php
+++ src/Upload/File.php
@@ -1,1 +1,19 @@
 <?php
+declare(strict_types=1);
+
+namespace Froq\File\Upload;
+
+use Froq\File\{File as FileBase, FileException};
+
+final class File extends FileBase
+{
+    final public function save(): bool
+    {
+        return move_uploaded_file($this->getSourceFile(), $this->getTargetFile());
+    }
+
+    final public function saveAs(string $target): bool
+    {
+        return move_uploaded_file($this->getSourceFile(), $target);
+    }
+}
