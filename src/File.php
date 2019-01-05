db2c2882c154963a9f5bfe7242fb999a4b0c638e
Kerem Güneş <k-gun@mail.com>
2019-01-05 05:11:13 +0300

    Move abstract methods to FileInterface object, make some properties & methods more semantic, add File::convertBytes(), add File::$options, update return types, make all properties readonly via Constructor, optimize.

--- src/File.php
+++ src/File.php
@@ -50,7 +50,7 @@ abstract class File
      * Tmp name.
      * @var string
      */
-    protected $nameTmp;
+    protected $tmpName;
 
     /**
      * Type.
@@ -71,35 +71,96 @@ abstract class File
     protected $extension;
 
     /**
-     * Error.
+     * Errors.
+     * @var array
+     */
+    protected static $errors = [
+        0 => null,
+        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
+        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
+        3 => 'The uploaded file was only partially uploaded.',
+        4 => 'No file was uploaded.',
+        6 => 'Missing a temporary folder.',
+        7 => 'Failed to write file to disk.',
+        8 => 'A PHP extension stopped the file upload.',
+    ];
+
+    /**
+     * Error code.
      * @var int
      */
-    protected $error;
+    protected $errorCode;
+
+    /**
+     * Error string.
+     * @var string
+     */
+    protected $errorString;
+
+    /**
+     * Options.
+     * @var array
+     */
+    protected $options = [
+        'hash' => null, // 'file' or 'fileName'
+        'hashAlgo' => null, // 'md5' or 'sha1' (default='md5')
+        'maxFileSize' => null, // no limit (default=php.ini)
+        'allowedExtensions' => [], // all allowed
+        'jpegQuality' => 80, // for image files
+    ];
 
     /**
      * Constructor.
+     * @param array  $file
      * @param string $directory
-     * @param array|null  $data
+     * @param array  $options
      */
-    public function __construct(string $directory, array $data = null)
+    public function __construct(array $file, string $directory, array $options = [])
     {
-        $this->setDirectory($directory);
+        if (!ini_get('file_uploads')) {
+            throw new FileException('File uploads now allowed by php.ini');
+        }
 
-        if (!empty($data)) {
-            isset($data['name']) &&
-                $this->setName($data['name']);
-            isset($data['type']) &&
-                $this->setType($data['type']) && $this->setExtension($data['type']);
-            isset($data['size']) &&
-                $this->setSize($data['size']);
+        // all these stuff are needed
+        if (!isset($file['tmp_name'], $file['name'], $file['type'], $file['size'], $file['error'])) {
+            throw new FileException("No valid file given, 'tmp_name,name,type,size,error' are required");
+        }
 
-            if (isset($data['tmp_name'])) {
-                $this->setNameTmp($data['tmp_name']);
-            } elseif (isset($data['source'])) {
-                $this->setNameTmp($data['source']);
+        // check file exists
+        $this->tmpName = $file['tmp_name'];
+        if (!is_file($this->tmpName)) {
+            throw new FileException("No file '{$this->tmpName}' found by 'tmp_name'");
+        }
+
+        // prepare options
+        $this->options = array_merge($this->options, $options + [
+            'maxFileSize' => self::convertBytes((string) ini_get('upload_max_filesize'))
+        ]);
+
+        $this->name = self::prepareName($file['name']);
+        $this->type = $file['type'];
+        $this->size = $file['size'];
+        $this->error = $file['error'] ?: null;
+        $this->errorString = $this->error != null ? self::$errors[$this->error] ?? 'Unknown.' : null;
+
+        $this->extension = Mime::getExtensionByType($this->type);
+        if (!empty($this->options['allowedExtensions'])
+            && !in_array($this->extension, (array) $this->options['allowedExtensions'])) {
+            throw new FileException("Extension '{$this->extension}' is not allowed");
+        }
+
+        // directory stuff
+        if ($directory == '') {
+            throw new FileException('Directory cannot be empty');
+        }
+
+        $this->directory = $directory;
+        if (!is_dir($this->directory)) {
+            @ $ok = mkdir($this->directory, 0644, true);
+            if (!$ok) {
+                throw new FileException(sprintf('Cannot make directory, error[%s]',
+                    error_get_last()['message'] ?? 'Unknown'));
             }
-
-            $this->error = $data['error'] ?? null;
         }
     }
 
@@ -112,163 +173,106 @@ abstract class File
     }
 
     /**
-     * Set directory.
-     * @param  string $directory
-     * @return self
-     */
-    public final function setDirectory(string $directory): self
-    {
-        if ($directory == '') {
-            throw new FileException('File directory cannot be empty');
-        }
-
-        $this->directory = $directory;
-        if (!is_dir($this->directory)) {
-            @ $ok = mkdir($this->directory, 0644, true);
-            if (!$ok) {
-                throw new FileException(sprintf('Cannot make directory, error[%s]',
-                    error_get_last()['message'] ?? 'Unknown'));
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * Get directory.
-     * @return ?string
-     */
-    public final function getDirectory(): ?string
-    {
-        return $this->directory;
-    }
-
-    /**
-     * Set name.
-     * @param  string $name
-     * @return self
-     */
-    public final function setName(string $name): self
-    {
-        $this->name = pathinfo($name, PATHINFO_FILENAME);
-
-        return $this;
-    }
-
-    /**
      * Get name.
-     * @return ?string
+     * @return string
      */
-    public final function getName(): ?string
+    public final function getName(): string
     {
         return $this->name;
     }
 
     /**
-     * Set name tmp.
-     * @param  string $nameTmp
-     * @return self
+     * Get tmp name.
+     * @return string
      */
-    public final function setNameTmp(string $nameTmp): self
+    public final function getTmpName(): string
     {
-        $this->nameTmp = $nameTmp;
-
-        return $this;
-    }
-
-    /**
-     * Get name tmp.
-     * @return ?string
-     */
-    public final function getNameTmp(): ?string
-    {
-        return $this->nameTmp;
-    }
-
-    /**
-     * Set type.
-     * @param  string $type
-     * @return self
-     */
-    public final function setType(string $type): self
-    {
-        $this->type = $type;
-
-        return $this;
+        return $this->tmpName;
     }
 
     /**
      * Get type.
-     * @return ?string
+     * @return string
      */
-    public final function getType(): ?string
+    public final function getType(): string
     {
         return $this->type;
     }
 
     /**
-     * Set extension.
-     * @param  string $type
-     * @return self
-     */
-    public final function setExtension(string $type): self
-    {
-        $this->extension = Mime::getExtensionByType($type);
-
-        return $this;
-    }
-
-    /**
      * Get extension.
-     * @return ?string
+     * @return string
      */
-    public final function getExtension(): ?string
+    public final function getExtension(): string
     {
         return $this->extension;
     }
 
     /**
-     * Set size.
-     * @param  int $size
-     * @return self
-     */
-    public final function setSize(int $size): self
-    {
-        $this->size = $size;
-
-        return $this;
-    }
-
-    /**
      * Get size.
-     * @return ?int
+     * @return int
      */
-    public final function getSize(): ?int
+    public final function getSize(): int
     {
         return $this->size;
     }
 
     /**
-     * Get source file.
-     * @return ?string
+     * Get error.
+     * @return ?int
      */
-    public final function getSourceFile(): ?string
+    public final function getError(): ?int
     {
-        return $this->nameTmp;
+        return $this->error;
     }
 
     /**
-     * Get target file.
+     * Get error string.
      * @return ?string
      */
-    public final function getTargetFile(): ?string
+    public function getErrorString(): ?string
     {
-        $sourceFile = $this->getSourceFile();
-        if ($sourceFile != null) {
-            return sprintf('%s/%s.%s', $this->directory, $this->name, $this->extension);
-        }
+        return $this->errorString;
+    }
 
-        return null;
+    /**
+     * Get directory.
+     * @return string
+     */
+    public final function getDirectory(): string
+    {
+        return $this->directory;
+    }
+
+    /**
+     * Get options.
+     * @return array
+     */
+    public final function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * Get source path.
+     * @return string
+     */
+    public final function getSourcePath(): string
+    {
+        return $this->tmpName;
+    }
+
+    /**
+     * Get destination path.
+     * @param  string|null $name
+     * @return string
+     */
+    public final function getDestinationPath(string $name = null): string
+    {
+        // safe name if provided
+        $name = $name === null ? $this->name : self::prepareName($name, false);
+
+        return sprintf('%s/%s.%s', $this->directory, $name, $this->extension);
     }
 
     /**
@@ -281,38 +285,73 @@ abstract class File
     }
 
     /**
-     * Save.
-     * @return bool
-     * @throws Froq\File\FileException
+     * To array.
+     * @return array
      */
-    abstract public function save(): bool;
+    public function toArray(): array
+    {
+        return [
+            'name'        => $this->name,
+            'tmp_name'    => $this->tmpName,
+            'type'        => $this->type,
+            'size'        => $this->size,
+            'error'       => $this->error,
+            'errorString' => $this->errorString
+        ];
+    }
 
     /**
-     * Save as.
+     * Prepare name.
      * @param  string $name
-     * @return bool
+     * @param  bool   $hash @internal
+     * @return string
      * @throws Froq\File\FileException
      */
-    abstract public function saveAs(string $name): bool;
+    public static function prepareName(string $name, bool $hash = null): string
+    {
+        // some security stuff
+        $name = preg_replace('~[^\w-.]~u', '', pathinfo($name, PATHINFO_FILENAME));
+        if (strlen($name) > 250) {
+            $name = substr($name, 0, 250);
+        }
+
+        // hash name if option set
+        $hash = $hash ?? $this->options['hash'];
+        if ($hash) {
+            $hashAlgo = $this->options['hashAlgo'] ?? 'sha1';
+            if (!in_array($hashAlgo, ['md5', 'sha1'])) {
+                throw new FileException("Only 'md5,sha1' algos accepted");
+            }
+
+            if ($hash == 'fileName') {
+                $name = call_user_func($hashAlgo, $this->tmpName);
+            } elseif ($hash == 'file') {
+                $name = call_user_func($hashAlgo .'_file', $this->tmpName);
+            }
+        }
+
+        return $name;
+    }
 
     /**
-     * Move.
-     * @return bool
-     * @throws Froq\File\FileException
+     * Conver bytes.
+     * @param  string $value
+     * @return int
      */
-    abstract public function move(): bool;
+    public static function convertBytes(string $value): int
+    {
+        if (!is_numeric($value)) {
+            $scan = sscanf($value, '%d%s');
+            if (isset($scan[1])) {
+                $value = (int) $value;
+                switch (strtoupper($scan[1])) {
+                    case 'K'; $value *= 1024; break;
+                    case 'M'; $value *= 1048576; break;
+                    case 'G'; $value *= 1073741824; break;
+                }
+            }
+        }
 
-    /**
-     * Move as.
-     * @param  string $name
-     * @return bool
-     * @throws Froq\File\FileException
-     */
-    abstract public function moveAs(string $name): bool;
-
-    /**
-     * Clear.
-     * @return void
-     */
-    abstract public function clear(): void;
+        return (int) $value;
+    }
 }
