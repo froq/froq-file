f7cfd038ffc3738dbc76f93bc7b7ddaea82be798
Kerem Güneş <qeremy@gmail.com>
2020-02-24 06:59:11 +0300

    Nope..

--- src/AbstractUploader.php
+++ src/AbstractUploader.php
@@ -80,8 +80,8 @@ abstract class AbstractUploader
      */
     public final function __construct(array $file, string $directory, array $options = null)
     {
-        @ ['type' => $type, 'name'  => $name, 'tmp_name' => $source,
-           'size' => $size, 'error' => $error] = $file;
+        @ ['type'  => $type, 'name'  => $name, 'tmp_name' => $source, 'size' => $size,
+           'error' => $error] = $file;
 
         // All these stuff are needed.
         if (!$type || !$name || !$source) {
