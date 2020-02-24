06fe5bb1406b1aa9a9ba21312bd087a8a0753551
Kerem Güneş <qeremy@gmail.com>
2020-02-24 06:58:14 +0300

    AbstractUploader: update constructor [drop array_fill_keys()].

--- src/AbstractUploader.php
+++ src/AbstractUploader.php
@@ -80,8 +80,8 @@ abstract class AbstractUploader
      */
     public final function __construct(array $file, string $directory, array $options = null)
     {
-        ['type' => $type, 'name' => $name, 'tmp_name' => $source, 'size' => $size, 'error' => $error]
-            = array_merge(array_fill_keys(['type', 'name', 'tmp_name', 'size', 'error'], null), $file);
+        @ ['type' => $type, 'name'  => $name, 'tmp_name' => $source,
+           'size' => $size, 'error' => $error] = $file;
 
         // All these stuff are needed.
         if (!$type || !$name || !$source) {
