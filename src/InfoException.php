5fb99363ab3ba9c3c045d37f731b154c1ef8b7e9
Kerem Güneş <qeremy@gmail.com>
2022-01-24 01:48:09 +0300

    Add Info,InfoException classes [missed].

---
+++ src/InfoException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-file
+ */
+declare(strict_types=1);
+
+namespace froq\file;
+
+/**
+ * Info Exception.
+ *
+ * @package froq\file
+ * @object  froq\file\InfoException
+ * @author  Kerem Güneş
+ * @since   6.0
+ */
+class InfoException extends FileException
+{}
